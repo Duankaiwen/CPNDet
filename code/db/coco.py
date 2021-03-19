98ae0d36c6b8e97fc21e0561732469c47d9452e2
Michal Ostyk <mostyk@dtiq.com>
2021-03-19 09:34:19 -0700

    to aws

--- code/db/coco.py
+++ code/db/coco.py
@@ -22,9 +22,9 @@ class MSCOCO(DETECTION):
 
         self._split = split
         self._dataset = {
-            "trainval": "trainval2014",
-            "minival": "minival2014",
-            "testdev": "testdev2017"
+            "trainval": "train",
+            "minival": "valid",
+            "testdev": "test"
         }[self._split]
         
         self._coco_dir = os.path.join(data_dir, "coco")
@@ -47,14 +47,7 @@ class MSCOCO(DETECTION):
         ], dtype=np.float32)
 
         self._cat_ids = [
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 
-            14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 
-            24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 
-            37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 
-            48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 
-            58, 59, 60, 61, 62, 63, 64, 65, 67, 70, 
-            72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 
-            82, 84, 85, 86, 87, 88, 89, 90
+            0
         ]
         self._classes = {
             ind + 1: cat_id for ind, cat_id in enumerate(self._cat_ids)
--- code/external/setup.py
+++ code/external/setup.py
@@ -6,8 +6,7 @@ from Cython.Build import cythonize
 extensions = [
     Extension(
         "nms", 
-        ["nms.pyx"],
-        extra_compile_args=["-Wno-cpp", "-Wno-unused-function"]
+        ["nms.pyx"]
     )
 ]
 
