6b3c108ca3180fad4579658c5dd5551e149fbc21
Ubuntu <ubuntu@ip-172-31-23-124.us-east-2.compute.internal>
2021-03-23 07:10:24 +0000

    finished training

--- code/models/py_utils/kp_utils.py
+++ code/models/py_utils/kp_utils.py
@@ -155,8 +155,8 @@ def _filter_bboxes(ht_boxes, tl_clses, region_scores, grouping_scores, gr_thresh
     
     pos_grouping_score_inds = grouping_scores >= gr_threshold
     ppos_ht_score_cls = pos_ht_score_cls[pos_grouping_score_inds]
-    
-    specific_rscores = region_scores.gather(1, ppos_ht_score_cls[:,1].long().unsqueeze(-1)).squeeze()
+    print(ppos_ht_score_cls.shape)
+    specific_rscores = region_scores.gather(1, ppos_ht_score_cls[:,1].long()).squeeze()
     #specific_rscores[specific_rscores<0.1] = 0
     
     ppos_ht_score_cls[:,0] = (ppos_ht_score_cls[:,0] + 0.5)* (specific_rscores + 0.5) - 0.25
