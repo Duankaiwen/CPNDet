52fe03c392f6d874718c4b8cf53ac431d9d41f8d
Ostyk <mostyk@dtiq.com>
2021-05-12 09:02:11 +0000

    results

---
+++ code/test_models.sh
@@ -0,0 +1,7 @@
+python test.py --cfg_file DLA34 --testiter 20000 --split testing
+python test.py --cfg_file DLA34 --testiter 20000 --split validation
+python test.py --cfg_file HG52 --testiter 30000 --split testing
+python test.py --cfg_file HG52 --testiter 30000 --split validation
+python test.py --cfg_file HG104 --testiter 30000 --split testing
+python test.py --cfg_file HG104 --testiter 30000 --split validation
+
